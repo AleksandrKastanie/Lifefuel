bc081de500abe0f8f1c0cad0c8a764afca604218
AleksandrKastanie <roof10023@gmail.com>
2023-05-05 15:34:38 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<script>
+import HeaderComp from '@/components/HeaderComp.vue'
+
+export default{
+  components:{
+    HeaderComp
+  }
+}
+</script>
+
+<template>
+  <header>
+    <HeaderComp />
+  </header>
+</template>
+
+<style>
+*,*::before,*::after{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+:root {
+    --font: 'Poppins', sans-serif;
+    --accent: #a26fcd;
+    --menu-background: #2a2a2acc;
+    --menu-title: #c3c3c3;
+    --menu-border: #3a3a3a9f;
+    --menu-hover: #e6e6e68a;
+    --cards-bg: #3f3f3f3f;
+    --cards-title: #e8e8e8;
+    --cards-text: #c3c3c3;
+    --cards-icon-bg: #aeaeae;
+    --inset-color: rgba(235, 225, 255, 0.178);
+  }
+  body {
+    height: 100svh;
+    color: white;
+    font-family: var(--font);
+    background: radial-gradient(55.65% 220.76% at 21.91% 46.07%, rgba(115, 41, 161, 0.197) 0, rgba(194, 97, 254, .04) 100%), #1d1d1d;
+  }
+
+  header{
+    background-color: var(--menu-background);
+    color: white;
+    border-style: solid;
+    border: 1px;
+    border-color: var(--menu-border);
+  }
+
+  @font-face{
+    font-family: 'Poppins';
+    src: url(/fonts/Poppins-Bold.ttf);
+    font-weight: 700;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'Popppins';
+    src: url(/fonts/Poppins-Light.ttf);
+    font-weight: 300;
+  }
+  @font-face {
+    font-family: 'Poppins';
+    src: url(/fonts/Poppins-Medium.ttf);
+    font-weight: 500;
+  }
+  @font-face {
+    font-family: 'Poppins';
+    src: url(/fonts/Poppins-Regular.ttf);
+    font-weight: 400;
+  }
+  @font-face {
+    font-family: 'Poppins';
+    src: url(/fonts/Poppins-SemiBold.ttf);
+    font-weight: 600;
+  }
+
+
+
+</style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,46 @@
+<script>
+    export default{
+        data(){
+            return{
+                pagecall:''
+            }
+        }
+    }
+</script>
+<template>
+    <div class="header-wrap">
+        <div class="left-btn-wrap"><button class="btn-show-page">Home</button></div>
+        <div class="right-btn-wrap">
+            <img class="notif-btn-pic" src="/icons/bell.badge.svg">
+            <img class="account-btn-pic" src="/photo/photo_2023-04-24_17-51-05.jpg">
+        </div>
+    </div>
+</template>
+<style>
+.header-wrap{
+    display: flex;
+    justify-content: space-around;
+}
+.left-btn-wrap{
+    display: flex;
+    align-self: center;
+}
+.right-btn-wrap{
+    display: flex;
+    gap: 4px 10px;
+    
+}
+.btn-show-page{
+    all: unset;
+}
+.notif-btn-pic{
+    border-radius: 50%;
+    width: 30px;
+    height: 30px;
+}
+.account-btn-pic{
+    border-radius: 50%;
+    width: 30px;
+    height: 30px;
+}
+</style>
